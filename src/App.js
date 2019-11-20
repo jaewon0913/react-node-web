75f1b34c09bb5b2eac5df37cd9bf06d0fd8caab4
jaewon0913 <jaewon9937@gmail.com>
2019-11-20 16:23:03 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Customer from './components/Customer';
 import './App.css';
 
-function App() {
-  return (
-    <div className="gray-background">
-        <img src={logo} alt="logo" />
-        <h2>리액트 프로젝트 시작</h2>
-    </div>
-  );
+const customers = [
+{
+  'id' : 1,
+  'name': '황재원',
+  'birthday': '930913',
+  'gender': '남자',
+  'job': '대학생'
+},
+{
+  'id' : 2,
+  'name': '이지은',
+  'birthday': '931011',
+  'gender': '여자',
+  'job': '연예인'
+},
+{
+  'id' : 3,
+  'name': '이순신',
+  'birthday': '961119',
+  'gender': '남자',
+  'job': '영웅'
+}
+]
+
+class App extends Component {
+  render(){
+    return (
+      <div>
+        {customers.map(c => {
+          return <Customer key = {c.id} id = {c.id} name = {c.name} birthday = {c.birthday} gender = {c.gender} job = {c.job} />
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
