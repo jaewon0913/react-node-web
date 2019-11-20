c9df85af797307bb089cc303dd4de70fa2d293f5
jaewon0913 <jaewon9937@gmail.com>
2019-11-20 15:35:11 +0900

    Create React_Node_WEB

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+### Management System
+
+본 프로젝트는 React 강의 목적으로 만들어진 고객 관리 시스템(Management System)입니다.  
+
+출처: https://ndb796.tistory.com/213?category=1030599 [안경잡이개발자]  
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -4,21 +4,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="gray-background">
+        <img src={logo} alt="logo" />
+        <h2>리액트 프로젝트 시작</h2>
     </div>
   );
 }
